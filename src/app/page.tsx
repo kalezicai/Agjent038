c43e98f35a10a8dadc0efac04fc1304e91ffa547
osoo420xl <okaleziq@gmail.com>
2026-08-04 03:15:08 +0200

    chore: clean up root page redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import { redirect } from "next/navigation";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Agjent038",
+};
 
 export default function RootPage() {
   redirect("/sq");
